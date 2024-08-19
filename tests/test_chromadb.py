adc10d7e18202d5794d26a726eefb9ec6aab2307
jaye <735740291@qq.com>
2024-08-19 11:08:03 +0800

    [update] update test files

--- tests/test_chromadb.py
+++ tests/test_chromadb.py
@@ -22,14 +22,16 @@ collection.add(
 #collection.delete(where={"source": "notion"})
 
 # Query/search 2 most similar results. You can also .get by id
-results = collection.query(
-    query_texts=["This is a query document"],
-    n_results=1,
-    # where={"metadata_field": "is_equal_to_this"}, # optional filter
-    # where_document={"$contains":"search_string"}  # optional filter
-)
 
-print(results)
+if __name__ == '__main__':
+    print('jaye')
+    results = collection.query(
+        query_texts=["This is a query document"],
+        n_results=1,
+        # where={"metadata_field": "is_equal_to_this"}, # optional filter
+        # where_document={"$contains":"search_string"}  # optional filter
+    )
+    print(results)
 
 
 
@@ -83,4 +85,3 @@ print(results)
 
 # print(collection.query(query_embeddings=hugging_face_embeddings.embed_query("RAG增强"), n_results=1))
 # print(collection.query(query_embeddings=hugging_face_embeddings.embed_query("FSDP算法是由来自DeepSpeed的ZeroRedundancyOptimizer技术驱动的，但经过修改的设计和实现与PyTorch的其他组件保持一致。FSDP将模型实例分解为更小的单元，然后将每个单元内的所有参数扁平化和分片。分片参数在计算前按需通信和恢复，计算结束后立即丢弃。这种方法确保FSDP每次只需要实现一个单元的参数，这大大降低了峰值内存消耗。"), n_results=1))
-
