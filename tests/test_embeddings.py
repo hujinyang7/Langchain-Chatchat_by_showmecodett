f93324d49fb2d2b4cf2344da970ec3bd509132ac
jaye <735740291@qq.com>
2024-08-19 15:05:08 +0800

    [add] add 02copy_config_example.py

--- tests/test_embeddings.py
+++ tests/test_embeddings.py
@@ -9,56 +9,45 @@ print('SentenceTransformer')
 from sentence_transformers import SentenceTransformer
 
 print('time to load model')
-
 model = SentenceTransformer(model_path)
 
 print('time to encode')
-#Our sentences we like to encode
 sentences = [
     '* Moka 此文本嵌入模型由 MokaAI 训练并开源，训练脚本使用 uniem',
     '* Massive 此文本嵌入模型通过**千万级**的中文句对数据集进行训练',
     '* Mixed 此文本嵌入模型支持中英双语的同质文本相似度计算，异质文本检索等功能，未来还会支持代码检索，ALL in one'
 ]
-
-#
-# #Sentences are encoded by calling model.encode()
-# sentence_transformer_embeddings = model.encode(sentences)
-#
-#
-#
-#
-#
-# print('HuggingFaceEmbeddings')
-# # test huggingface embeddings
-# MODEL_ROOT_PATH = os.path.join(os.path.dirname(os.path.dirname(__file__)), "_models")
-#
-# # 选用的 Embedding 名称
-# EMBEDDING_MODEL = "m3e-base"
-#
-# from langchain.embeddings.huggingface import HuggingFaceEmbeddings
-# hugging_face_embeddings = HuggingFaceEmbeddings(model_name=f'{MODEL_ROOT_PATH}/moka-ai/{EMBEDDING_MODEL}')
+sentence_transformer_embeddings = model.encode(sentences)
 
 
 
+print('test huggingface embeddings')
+MODEL_ROOT_PATH = os.path.join(os.path.dirname(os.path.dirname(__file__)), "_models")
+EMBEDDING_MODEL = "m3e-base"   # 选用的 Embedding 名称
 
-# if __name__ == '__main__':
-#     # 在线运行
-#     from sentence_transformers import SentenceTransformer
-#
-#     model = SentenceTransformer('moka-ai/m3e-base')
-#
-#     # Our sentences we like to encode
-#     sentences = [
-#         '* Moka 此文本嵌入模型由 MokaAI 训练并开源，训练脚本使用 uniem',
-#         '* Massive 此文本嵌入模型通过**千万级**的中文句对数据集进行训练',
-#         '* Mixed 此文本嵌入模型支持中英双语的同质文本相似度计算，异质文本检索等功能，未来还会支持代码检索，ALL in one'
-#     ]
-#
-#     # Sentences are encoded by calling model.encode()
-#     embeddings = model.encode(sentences)
-#
-#     # Print the embeddings
-#     for sentence, embedding in zip(sentences, embeddings):
-#         print("Sentence:", sentence)
-#         print("Embedding:", embedding)
-#         print("")
+
+from langchain.embeddings.huggingface import HuggingFaceEmbeddings
+hugging_face_embeddings = HuggingFaceEmbeddings(model_name=f'{MODEL_ROOT_PATH}/moka-ai/{EMBEDDING_MODEL}')
+
+
+def compare_float_arrays(arr1, arr2):
+    if len(arr1) != len(arr2):
+        return False
+    for a, b in zip(arr1, arr2):
+        if round(a, 2) != round(b, 2):
+            return False
+    return True
+
+
+for i in range(len(sentences)):
+    print("Sentence:", sentences[i])
+    print("SentenceTransformer:", sentence_transformer_embeddings[i][:5])
+    print('is_same', compare_float_arrays(sentence_transformer_embeddings[i], hugging_face_embeddings.embed_query(sentences[i])))
+    print("")
+
+print('success')
+
+
+
+
+
